9c262defa80b7a2081b4e1bbf5b7eccd0e7d5527
mansour9897 <mansour9897@gmail.com>
2023-11-06 10:26:45 +0330

    create project

---
+++ MVVM/MVVM/GenericRelayCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MVVM
+{
+	public class GenericRelayCommand<T> : ICommand
+	{
+		public event EventHandler CanExecuteChanged;
+		Action<object> action;
+		public GenericRelayCommand(Action<object> action)
+		{
+			this.action = action;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+
+		public void Execute(object parameter)
+		{
+			//if (parameter == null) return;
+			action(parameter);
+		}
+	}
+}
